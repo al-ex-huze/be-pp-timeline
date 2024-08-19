e2191ce11d5b52ae6baa567b80fa08b763b62df2
Alex Hughes <al.ex.hughes@outlook.com>
2024-08-19 16:50:54 +0100

    implements 500 error handling

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const app = express();
 
 const { getEndpoints } = require("./controllers/api.controller.js");
 
+const { handleServerErrors } = require("./errors/app.errors.js");
+
 app.get("/api", getEndpoints);
 
+app.use(handleServerErrors);
 app.all("*", (req, res) => {
     res.status(404).send({ msg: "route not found" });
 });
