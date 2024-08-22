2f62a4ab551058f7a7c02a68bdfa1a4970bea90e
Alex Hughes <al.ex.hughes@outlook.com>
2024-08-22 15:22:29 +0100

    implements event date patch and tests

--- controllers/events.controller.js
+++ controllers/events.controller.js
@@ -1,39 +1,55 @@
-const { selectEvents, selectEventByID, insertEvent, deleteEvent } = require("../models/events.model.js");
+const {
+    selectEvents,
+    selectEventByID,
+    insertEvent,
+    deleteEvent,
+    updateEventDates,
+} = require("../models/events.model.js");
 
 exports.getEvents = (req, res, next) => {
     selectEvents()
-    .then((events) => {
-        res.status(200).send({ events });
-    })
-    .catch(next);
+        .then((events) => {
+            res.status(200).send({ events });
+        })
+        .catch(next);
 };
 
 exports.getEventByID = (req, res, next) => {
     const { event_id } = req.params;
     selectEventByID(event_id)
-    .then((event) => {
-        res.status(200).send({ event });
-    })
-    .catch(next);
-}
+        .then((event) => {
+            res.status(200).send({ event });
+        })
+        .catch(next);
+};
 
 exports.createEvent = (req, res, next) => {
     insertEvent(req.body)
-    .then((event_id) => {
-        return selectEventByID(event_id.toString());
-    })
-    .then((event) => {
-        res.status(201);
-        res.send({ event });
-    })
-    .catch(next);
-}
+        .then((event_id) => {
+            return selectEventByID(event_id.toString());
+        })
+        .then((event) => {
+            res.status(201);
+            res.send({ event });
+        })
+        .catch(next);
+};
 
 exports.removeEvent = (req, res, next) => {
     const { event_id } = req.params;
     deleteEvent(event_id)
-    .then(() => {
-        res.status(204).send();
-    })
-    .catch(next);
-}
+        .then(() => {
+            res.status(204).send();
+        })
+        .catch(next);
+};
+
+exports.patchEventDates = (req, res, next) => {
+    const { event_id } = req.params;
+
+    updateEventDates(req.body, event_id)
+        .then((event) => {
+            res.status(200).send({ event });
+        })
+        .catch(next);
+};
