e3c0e819f7e1c755d6fd762713c555c4345c38b4
Alex Hughes <al.ex.hughes@outlook.com>
2024-08-28 18:26:57 +0100

    removes type from package.json

---
+++ controllers/gh.controller.js
@@ -0,0 +1,7 @@
+// const { getWeeklyCommitsCall } = require("../ghapi.js");
+
+// exports.getWeeklyCommits = (res, req, next) => {
+//     getWeeklyCommitsCall().then((response) => {
+//         console.log("response in API")
+//     })
+// }   
