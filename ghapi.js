9055e0da2cd4d81d30bb41a6363dad779ea645cd
Alex Hughes <al.ex.hughes@outlook.com>
2024-08-28 19:23:30 +0100

    amends api and error

--- ghapi.js
+++ ghapi.js
@@ -1,7 +1,13 @@
 const axios = require("axios");
-const dotenv = require("dotenv");
+const ENV = process.env.NODE_ENV || "development";
 
-dotenv.config();
+require("dotenv").config({
+    path: `${__dirname}/.env.${ENV}`,
+});
+
+if (!process.env.GH_TOKEN) {
+    throw new Error("GH_TOKEN not set");
+}
 
 const user = "al-ex-huze";
 const repo = "be-pp-timeline";
@@ -11,19 +17,22 @@ const ghApi = axios.create({
 });
 
 exports.getGhApiCb = (req, res, next) => {
-    getGhApi().then((response) => {
-        console.log(response);
-        res.status(200).send({ response });
-    })
-    .catch(next);
+    getGhApi()
+        .then((response) => {
+            console.log(response);
+            res.status(200).send(response);
+        })
+        .catch(next);
 };
 
 const getGhApi = () => {
     return ghApi
         .get(`/repos/${user}/${repo}`, {
             headers: {
+                Accept: "application/vnd.github+json",
                 Authorization: `Bearer ${process.env.GH_TOKEN}`,
                 "Content-Type": "application/json",
+                "X-GitHub-Api-Version": "2022-11-28",
             },
             // auth: {}
         })
